8f4ad16df1f4e0a19b6766cdce36ff16add39119
codehunter <hunterx6323@gmail.com>
2022-07-31 10:06:33 +0700

    init project

---
+++ src/main/java/com/codehunter/springmvcjpathymeleafliquibasepractice/SpringMvcJpaThymeleafLiquibasePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codehunter.springmvcjpathymeleafliquibasepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJpaThymeleafLiquibasePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJpaThymeleafLiquibasePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehunter/springmvcjpathymeleafliquibasepractice/SpringMvcJpaThymeleafLiquibasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehunter.springmvcjpathymeleafliquibasepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcJpaThymeleafLiquibasePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
